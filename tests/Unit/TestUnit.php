add8da2591e505c34051c573f49486674af88bc1
masqueradis <masqueradisu@gmail.com>
2026-03-22 21:37:30 +0300

    Fixed .gitignore

---
+++ tests/Unit/TestUnit.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class TestUnit
+{
+    //
+}
