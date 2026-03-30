e90f346734d82171966dc81d197fca8498d9fa2d
Masqueradis <masqueradisu@gmail.com>
2026-03-30 22:44:42 +0300

    Design of databases and models implemented (#37)

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,19 +11,19 @@ Route::get('/', function () {
     return redirect()->route('login');
 });
 
-Route::get('login', function() {
+Route::get('login', function () {
     return view('auth.login');
 })->name('login')->middleware('guest');
 
-Route::get('register', function() {
+Route::get('register', function () {
     return view('auth.register');
 })->name('register')->middleware('guest');
 
-Route::get('dashboard', function() {
+Route::get('dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified']);
 
-Route::get('email/verify', function() {
+Route::get('email/verify', function () {
     return view('auth.verify-email');
 })->middleware('auth')->name('verification.notice');
 
