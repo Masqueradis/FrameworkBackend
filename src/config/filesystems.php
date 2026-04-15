79ccf7332fb92db0b36341c2fbf62ad86d2026d5
masqueradis <masqueradisu@gmail.com>
2026-04-16 01:30:27 +0300

    MinIO infrastucture implemented

--- src/config/filesystems.php
+++ src/config/filesystems.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Str;
+
 return [
 
     /*
@@ -59,6 +61,17 @@ return [
             'throw' => false,
             'report' => false,
         ],
+
+        'minio' =>
+        [
+            'driver' => 's3',
+            'key' => env('MINIO_ROOT_USER'),
+            'secret' => env('MINIO_ROOT_PASSWORD'),
+            'endpoint' => env('MINIO_ENDPOINT'),
+            'bucket' => Str::slug(Str::lower(env('APP_NAME'), '-')) . '-minio',
+            'use_path_style_endpoint' => true,
+            'region' => env('AWS_DEFAULT_REGION', 'eu-central-1'),
+        ]
 
     ],
 
---
+++ src/tests/Feature/StorageTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+
+class StorageTest extends TestCase
+{
+    #[Test]
+    public function testCanWriteAndReadFileToMinio(): void
+    {
+        Storage::disk('minio')->put('example.txt', 'Hello World');
+        $content = Storage::disk('minio')->get("example.txt");
+
+        Storage::disk('minio')->assertExists('example.txt');
+        $this->assertStringContainsString('Hello World', $content);
+    }
+}
