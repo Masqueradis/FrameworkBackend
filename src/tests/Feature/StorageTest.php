3b2378d8350847ad0c4c89fd455678636a552dae
Masqueradis <masqueradisu@gmail.com>
2026-04-30 04:13:34 +0300

    Github actions fix

--- src/tests/Feature/StorageTest.php
+++ src/tests/Feature/StorageTest.php
@@ -13,6 +13,7 @@ class StorageTest extends TestCase
     #[Test]
     public function testCanWriteAndReadFileToMinio(): void
     {
+        Storage::fake('minio');
         Storage::disk('minio')->put('example.txt', 'Hello World');
         $content = Storage::disk('minio')->get("example.txt");
 
