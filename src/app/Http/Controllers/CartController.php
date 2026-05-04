c83ba999dd02ae81e3ee499ea8433a29763b18a4
Masqueradis <masqueradisu@gmail.com>
2026-05-04 13:24:58 +0300

    Cart-interface implemented

--- src/app/Http/Controllers/CartController.php
+++ src/app/Http/Controllers/CartController.php
@@ -11,6 +11,7 @@ use App\Services\CartService;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class CartController extends ApiController
 {
@@ -18,14 +19,25 @@ class CartController extends ApiController
         private readonly CartService $cartService,
     ) {}
 
-    public function add(AddToCartDTO $dto): JsonResponse
+    public function index(): View
+    {
+        $cart = $this->cartService->getCart();
+
+        $cart->load('items.product');
+
+        $total = $this->cartService->calculateTotal($cart);
+
+        return view('cart.index', compact('cart', 'total'));
+    }
+
+    public function add(AddToCartDTO $dto): RedirectResponse
     {
         $this->cartService->addItem($dto);
 
-        return $this->respondSuccess(null, 'Item added to cart.');
+        return back()->with('success', 'Item added to cart.');
     }
 
-    public function update(Request $request, CartItem $cartItem): JsonResponse
+    public function update(Request $request, CartItem $cartItem): RedirectResponse
     {
         $dto = UpdateCartItemDTO::from([
             'cartItemId' => $cartItem->id,
@@ -34,13 +46,13 @@ class CartController extends ApiController
 
         $this->cartService->updateItemQuantity($dto);
 
-        return $this->respondSuccess(null, 'Cart updated.');
+        return back()->with('success', 'Cart updated.');
     }
 
-    public function remove(CartItem $cartItem): JsonResponse
+    public function remove(CartItem $cartItem): RedirectResponse
     {
         $this->cartService->removeItem($cartItem->id);
 
-        return $this->respondSuccess(null, 'Item removed from cart.');
+        return back()->with('success', 'Item removed from cart.');
     }
 }
