6f9db290cb009df4cdcd6e5758683fd8adf92fc8
Masqueradis <masqueradisu@gmail.com>
2026-06-22 02:09:08 +0300

    12

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -63,6 +63,7 @@ class User extends Authenticatable implements OAuthenticatable, MustVerifyEmail
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
             'status' => UserStatus::class,
+            'google2fa_secret' => 'encrypted',
         ];
     }
 
